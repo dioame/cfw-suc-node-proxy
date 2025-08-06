d0b8d84f4867805a1feec2de4bd05428db1d547e
Jowam <69232719+dioame@users.noreply.github.com>
2025-08-06 09:57:27 +0800

    init

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-// server.js
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
 
 app.use(
   "/",
   createProxyMiddleware({
-    target: "http://cfw-suc.caraga-apps.com",
+    target: "https://cfw-suc.caraga-apps.com",
     changeOrigin: true,
+    pathRewrite: {
+      "^/": "/", // optional if you're not adding a base path
+    },
   })
 );
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Proxy server running on port 3000");
+});
